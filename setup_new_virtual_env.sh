d616c3abbcac43a95f7b8f5345516389ac480ae9
Caliskan <ac01342@balin.eps.surrey.ac.uk>
2020-06-01 11:18:18 +0100

    anaconda setup is added

---
+++ setup_new_virtual_env.sh
@@ -0,0 +1,6 @@
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda3 -s
+#rm Miniconda3-latest-Linux-x86_64.sh
+conda=./miniconda3/bin/conda
+$conda create -y -n pytorch python=3.7 pip
+$conda install -n pytorch -y pytorch torchvision cudatoolkit=10.1 -c pytorch
