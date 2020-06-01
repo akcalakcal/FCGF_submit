95d6499602661e6c89c76efad0e7069371f15397
akincaliskanR7 <akinafl@gmail.com>
2020-06-01 15:34:33 +0100

    small changes

--- run_single_pcd_FCGF_ext.sh
+++ run_single_pcd_FCGF_ext.sh
@@ -5,6 +5,6 @@ mainFolder=/vol/research/ucdatasets/MPSD/akin_tmp/src/
 sourcePath=${mainFolder}/FCGF_submit/
 dataFolder=${mainFolder}/FCGF_submit/data_test/tmp/
 featureExtFolder=${mainFolder}/FCGF_submit/features_tmp/
-trainedModel=${mainFolder}/FCGF_submit/outputs_trained_models/checkpoint.pth
+trainedModel=${mainFolder}/outputs_trained_models/checkpoint.pth
 
 ${pythonPath}/python ${sourcePath}/scripts/single_pcd_feature_extraction.py --source_file ${dataFolder}/7-scenes-redkitchen/cloud_bin_0.ply --target ${featureExtFolder}/7-scenes-redkitchen/ --voxel_size 0.025 --model ${trainedModel} --with_cuda
--- run_train_FCGF.sh
+++ run_train_FCGF.sh
@@ -1,6 +1,9 @@
 pythonPath=/vol/research/ucdatasets/MPSD/akin_tmp/miniconda3/envs/pytorch/bin/
-sourcePath=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/
-dataDir=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/data/threedmatch/
-outDir=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/outputs
+
+mainFolder=/vol/research/ucdatasets/MPSD/akin_tmp/src/
+
+sourcePath=${mainFolder}/FCGF_submit/
+dataDir=${mainFolder}/FCGF_submit/threedmatch/
+outDir=${mainFolder}/FCGF_submit/outputs
 
 ${pythonPath}/python ${sourcePath}/train.py --threed_match_dir ${dataDir} --out_dir ${outDir}
