bfdea2255ba6dfe6d36703a330a8b4d1a486efde
akincaliskanR7 <akinafl@gmail.com>
2020-06-01 14:52:18 +0100

    small changes

--- run_FCGF_feature_extractor.sh
+++
@@ -1,7 +0,0 @@
-pythonPath=/vol/research/ucdatasets/MPSD/akin_tmp/miniconda3/envs/pytorch/bin/
-sourcePath=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/
-dataFolder=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/data_test/tmp/
-featureExtFolder=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/features_tmp/
-trainedModel=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/outputs_trained_models/checkpoint.pth
-
-${pythonPath}/python ${sourcePath}/scripts/benchmark_3dmatch.py --source ${dataFolder} --target ${featureExtFolder} --voxel_size 0.025 --model ${trainedModel} --extract_features --evaluate_feature_match_recall --with_cuda
--- run_single_pcd_FCGF_ext.sh
+++ run_single_pcd_FCGF_ext.sh
@@ -1,7 +1,10 @@
 pythonPath=/vol/research/ucdatasets/MPSD/akin_tmp/miniconda3/envs/pytorch/bin/
-sourcePath=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/
-dataFolder=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF/data_test/tmp/
-featureExtFolder=/vol/research/ucdatasets/MPSD/akin_tmp/src/FCGF_submit/features_tmp/
-trainedModel=/vol/research/ucdatasets/MPSD/akin_tmp/src/outputs_trained_models/checkpoint.pth
+
+mainFolder=/vol/research/ucdatasets/MPSD/akin_tmp/src/
+
+sourcePath=${mainFolder}/FCGF_submit/
+dataFolder=${mainFolder}/FCGF_submit/data_test/tmp/
+featureExtFolder=${mainFolder}/FCGF_submit/features_tmp/
+trainedModel=${mainFolder}/FCGF_submit/outputs_trained_models/checkpoint.pth
 
 ${pythonPath}/python ${sourcePath}/scripts/single_pcd_feature_extraction.py --source_file ${dataFolder}/7-scenes-redkitchen/cloud_bin_0.ply --target ${featureExtFolder}/7-scenes-redkitchen/ --voxel_size 0.025 --model ${trainedModel} --with_cuda
