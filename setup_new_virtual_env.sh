1aa71974f901f3482d000dc9b846e3fe16ab1fc6
akin caliskan <akinafl@gmail.com>
2020-06-27 16:06:58 +0100

    gitignore added

--- setup_new_virtual_env.sh
+++ setup_new_virtual_env.sh
@@ -1,6 +1,6 @@
 #wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda3 -s
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/akin/workspace/workspace_dependencies/conda/miniconda3 -s
 #rm Miniconda3-latest-Linux-x86_64.sh
-conda=./miniconda3/bin/conda
+conda=/home/akin/workspace/workspace_dependencies/conda/miniconda3/bin/conda
 $conda create -y -n pytorch python=3.7 pip
 $conda install -n pytorch -y pytorch torchvision cudatoolkit=10.1 -c pytorch
